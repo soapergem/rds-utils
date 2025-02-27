b7f10bf75cfd0722c2d94305deff42210dc3eeec
Gordon Myers <soapergem@gmail.com>
2025-02-27 09:15:33 -0600

    reformatting

--- rds_utils/__init__.py
+++ rds_utils/__init__.py
@@ -1,3 +1,8 @@
-from rds_utils.utils import execute_query, fetch_query, format_parameters, get_xacct_session
+from rds_utils.utils import (
+    execute_query,
+    fetch_query,
+    format_parameters,
+    get_xacct_session,
+)
 
 __all__ = ["execute_query", "fetch_query", "format_parameters", "get_xacct_session"]
